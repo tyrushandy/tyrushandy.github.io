14e9e030149e7a418e837ff48087ce5adcd3c659
Eon John <eonnigeljohn@gmail.com>
2021-03-08 15:38:25 -0500

    updated resume name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getResume() {
   const el = document.createElement('a');
-  el.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent("Tyrus Handy Resume"));
+  el.setAttribute('href', 'tyrus-handy-resume.pdf');
   el.setAttribute('download', "tyrus-handy-resume.pdf");
   el.style.display = 'none';
   document.body.appendChild(el);
